692c0333b8968b3865afa71b4c23341e3b9d4703
Dpipeto <dpipegarzon@gmail.com>
2025-11-18 21:24:58 -0500

    fixtable

---
+++ database/migrations/0001_01_01_000003_create_users_proper_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        if (!Schema::hasTable('Users')) {
+            Schema::create('Users', function (Blueprint $table) {
+                $table->id('IdUsers');
+                $table->string('FirstName');
+                $table->string('LastName');
+                $table->string('Email')->unique();
+                $table->string('Password');
+                $table->rememberToken();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('Users');
+    }
+};
